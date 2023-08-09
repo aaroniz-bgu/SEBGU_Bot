0aa2f1af597304740766df90caa69a5fa08df684
aaroniz-bgu <aaroni@post.bgu.ac.il>
2023-08-10 00:40:34 +0300

    Overdoing everything

--- src/main/java/org/aaroniz/sebgu/SEBGUBot.java
+++ src/main/java/org/aaroniz/sebgu/SEBGUBot.java
@@ -1,35 +1,87 @@
 package org.aaroniz.sebgu;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
-import net.dv8tion.jda.api.sharding.ShardManager;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.interactions.commands.Command;
+import org.aaroniz.sebgu.commands.CommandInitializer;
+import org.aaroniz.sebgu.listeners.CommandListener;
 
 import javax.security.auth.login.LoginException;
+import java.util.List;
 
 public class SEBGUBot {
-    private final ShardManager shardManager;
+    private final Dotenv DOTENV;
+    private final JDA BOT;
 
-    public SEBGUBot() throws LoginException {
-        Dotenv env = Dotenv.configure().load();
-        String token = env.get("TOKEN");
+    private SEBGUBot() throws LoginException, InterruptedException {
+        //Loading up the environment
+        DOTENV = Dotenv.configure()
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
 
-        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
-        builder.setActivity(Activity.listening("Boker Tov Mike"));
-        builder.setStatus(OnlineStatus.ONLINE);
-        shardManager = builder.build();
+        //Token retrieval
+        String token = DOTENV.get("TOKEN");
+
+        //Building the bot.
+        //DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        BOT = JDABuilder.createDefault(token)
+                .setActivity(Activity.listening("Boker Tov Mike"))
+                .setStatus(OnlineStatus.ONLINE)
+                .build()
+                .awaitReady();
+
+        CommandInitializer initCommands = new CommandInitializer(BOT);
+        CommandListener listener = new CommandListener(initCommands);
+        BOT.addEventListener(listener);
     }
 
-    public ShardManager getShardManager(){
-        return shardManager;
+    /**
+     * @return the bot instance.
+     */
+    public JDA getJDA(){
+        return BOT;
+    }
+
+    /**
+     * @return DOTENV instance
+     */
+    public Dotenv getDotenv() {
+        return DOTENV;
     }
 
     public static void main(String[] args) {
         try {
-            SEBGUBot bot = new SEBGUBot();
-        } catch(LoginException e) {
-            System.out.println("Token is either invalid or incorrect - login failed!");
+            SEBGUBot segbu = new SEBGUBot();
+        } catch(LoginException | InterruptedException e) {
+            System.out.println("""
+                    Token is either invalid or incorrect - login failed!
+                    Please make sure that .env file is placed in resources directory
+                    Make sure the TOKEN variable is correct within the environment.
+                    """);
+        }
+    }
+
+
+    private void removeAllCommands() {
+        List<Guild> guilds = getJDA().getGuilds();
+        for(Guild g : guilds) {
+            List<Command> commands = g.retrieveCommands().complete();
+            for(Command command : commands) {
+                g.deleteCommandById(command.getId()).queue();
+            }
+        }
+
+        for(Guild g : guilds) {
+            System.out.println(g.retrieveCommands()
+                    .complete()
+                    .stream()
+                    .map(command -> command.getName())
+                    .toList());
         }
     }
 }
--- src/main/java/org/aaroniz/sebgu/commands/SlashCommand.java
+++ src/main/java/org/aaroniz/sebgu/commands/SlashCommand.java
@@ -1,16 +1,39 @@
 package org.aaroniz.sebgu.commands;
 
-import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import org.aaroniz.sebgu.SEBGUBot;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
 import java.util.List;
 
 public interface SlashCommand {
+
+    /**
+     * @return command's name.
+     */
     public String getName();
-    public String getDescription();
-    public List<OptionData> getOptions();
-    public String execute(SlashCommandInteractionEvent event);
-    public void rebuild(SEBGUBot bot);
+
+    /**
+     * @return the ids of the guilds this command should work at.
+     */
+    public List<Long> getGuilds();
+
+    /**
+     * @return the command for registering if required.
+     */
+    public SlashCommandData getSlashCommandData();
+
+    /**
+     * Adds the commands to the mapping of the bot which makes it callable and ready for execution.
+     * @param addCommand AddCommand callback from CommandInitializer.class.
+     * @return the command itself.
+     */
+
+    SlashCommand initCommand(AddCommand addCommand);
+
+    /**
+     * Executes the command's functionality.
+     * @param event the event that triggered the command.
+     */
+
+    public void execute(SlashCommandInteractionEvent event);
 }
